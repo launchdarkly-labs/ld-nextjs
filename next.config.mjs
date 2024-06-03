ad90b28e74bff1ab695d8e7b9843007173240dc2
Yusinto Ngadiman <yusinto@gmail.com>
2024-06-03 12:57:57 -0700

    chore: Remove debug throttle code.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-import { start } from '@sitespeed.io/throttle';
-
 export default async (phase, { defaultConfig }) => {
   const ld = await import('@launchdarkly/node-server-sdk');
 
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const options = { up: 360, down: 400, rtt: 200 };
-    await start(options);
     return {
       serverRuntimeConfig: {
         // Will only be available on the server side
