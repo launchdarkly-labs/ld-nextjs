af75f598cdc7b74cabb1982c014a64644c41ded6
Yusinto Ngadiman <yusinto@gmail.com>
2024-06-13 18:56:02 -0700

    fix: Return global ldClient on server.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import LDButton from '@/app/LDButton';
+import { useLDClient } from '@/ld/hooks';
 
 export default async function Home() {
+  const ldc = useLDClient();
+  const flagValue = await ldc?.variation('dev-test-flag');
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is Home. Below is LDButton.
+      From server, flagValue is {flagValue ? 'true' : 'false'}. <br />
+      Below is LDButton.
       <LDButton />
     </main>
   );
--- ld/hooks/useLDClient.ts
+++ ld/hooks/useLDClient.ts
@@ -1,8 +1,15 @@
+import { isServer } from '@/ld/isServer';
+import NextClient from '@/ld/nextClient';
 import { useContext } from 'react';
 
 import { context, type ReactContext } from '../provider/reactContext';
 
 const useLDClient = () => {
+  if (isServer) {
+    return NextClient.get();
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { client } = useContext<ReactContext>(context);
   return client;
 };
